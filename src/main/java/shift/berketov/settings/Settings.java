9bab9d3c86e4ca0e653585db1ec6077f1c3e386c
Sergio <Sergio_berketov@mail.ru>
2025-09-01 20:13:14 +0700

    Settings modified

--- src/main/java/shift/berketov/settings/Settings.java
+++ src/main/java/shift/berketov/settings/Settings.java
@@ -1,9 +1,7 @@
 package shift.berketov.settings;
 
-import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
